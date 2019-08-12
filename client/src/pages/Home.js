9c25eec07427b45d37dfe95b472c6000cca12545
Clark Tyra <clark.tyra@gmail.com>
2019-08-12 13:07:37 -0700

    set up outline structure for home page, added toggle function, cards, 3 column cards, today's workout button, exercise tidbit toggle, and statses for currentDaysInRow, highestDaysInRow, and totalDaysInRow

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,30 +1,96 @@
-import React, {Component } from 'react';
+import React, { Component } from 'react';
 import withAuth from './../components/withAuth';
 import API from './../utils/API';
 import { link } from 'react-router-dom';
+import { Collapse, Button, CardBody, Card } from 'reactstrap';
+
 
 class Home extends Component {
-    state = {
-        username: "",
-        email: ""
-      };
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: "",
+      email: "",
+      currentDaysInRow: 1,
+      highestDaysInRow: 2,
+      totalDaysInRow: 3,
+      collapse: false
+    };
+    this.toggle = this.toggle.bind(this);
+  }
+  
 
-      componentDidMount() {
-        API.getUser(this.props.user.id).then(res => {
-          this.setState({
-            username: res.data.username,
-            email: res.data.email
-          })
-        });
-      }
+  componentDidMount() {
+    API.getUser(this.props.user.id).then(res => {
+      this.setState({
+        username: res.data.username,
+        email: res.data.email
+      })
+    });
+  }
+  
+  toggle() {
+    this.setState( {collapse: !this.state.collapse });
+  }
 
-    render(){
-        return (
-            <div>
-                <h1>Welcome {this.state.username} to your home page</h1>
+  render() {
+
+    return (
+      <div>
+        <h1>Welcome {this.state.username} to your home page</h1>
+        <div class="container">
+          <div class="row">
+
+            <div class="col-sm">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">{this.state.currentDaysInRow}</h5>
+                  <p class="card-text"> You have done a burst workout {this.state.currentDaysInRow} days in a row
+                  </p>
+                </div>
+              </div>
             </div>
-        )
-    }
+
+            <div class="col-sm">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">{this.state.highestDaysInRow}</h5>
+                  <p class="card-text"> Your workout streak is {this.state.highestDaysInRow} days
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-sm">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">{this.state.totalDaysInRow}</h5>
+                  <p class="card-text"> Your total number of bust workouts are {this.state.totalDaysInRow}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+
+        <button type="button" className="btn btn-danger">Click here for today's workout</button>
+        <br/>
+        <br/>
+
+        <Button color="danger" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Exercise tidbit of the day!</Button>
+        <Collapse isOpen={this.state.collapse}>
+          <Card>
+            <CardBody>
+            It is not about how hard you workout today. Its how consistent you have been in a lifetime.
+            </CardBody>
+          </Card>
+        </Collapse>
+        
+
+      </div>
+    )
+  }
 }
 
 export default withAuth(Home);
