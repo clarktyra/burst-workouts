82caf0adf272671630dd809ee71c2a4cd8a54257
Clark Tyra <clark.tyra@gmail.com>
2019-08-15 13:01:45 -0700

    added Home, Workout, Leaderboard, Feedback, and Settings links to navbar, make the burst workouts sign link to home

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -13,7 +13,19 @@ class Navbar extends Component {
             return (
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <Link className="nav-link" to="/profile">Profile</Link>
+                        <Link className="nav-link" to="/home">Home</Link>
+                    </li>
+                    <li>
+                        <Link className="nav-link" to="/workout">Workout</Link>
+                    </li>
+                    <li>
+                        <Link className="nav-link" to="/leaderboard">Leaderboard</Link>
+                    </li>
+                    <li>
+                        <Link className="nav-link" to="/profile">Feedback</Link>
+                    </li>
+                    <li>
+                        <Link className="nav-link" to="/profile">Settings</Link>
                     </li>
                     <li className="nav-item">
                         {/* this is not using the Link component to logout or user and then refresh the application to the start */}
@@ -37,9 +49,9 @@ class Navbar extends Component {
 
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-danger">
                 <div className="container">
-                    <Link className="navbar-brand" to="/">React JWT App</Link>
+                    <Link className="navbar-brand" to="/home">Burst Workouts</Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
