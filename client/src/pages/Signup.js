0ae9bbabdc39eccc79ca88ec2463a7d7335e25d6
Clark Tyra <clark.tyra@gmail.com>
2019-08-16 14:06:26 -0700

    made the signup container a bootstrap grid centered column

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import AuthService from './../components/AuthService';
 import API from './../utils/API';
+import './Signup.css'
 
 class Signup extends Component {
   constructor() {
@@ -35,40 +36,48 @@ class Signup extends Component {
 
   render() {
     return (
-      <div className="container">
-
-        <h1>Signup</h1>
-        <form onSubmit={this.handleFormSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username:</label>
-            <input className="form-control"
-              placeholder="Username goes here..."
-              name="username"
-              type="text"
-              id="username"
-              onChange={this.handleChange} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email address:</label>
-            <input className="form-control"
-              placeholder="Email goes here..."
-              name="email"
-              type="email"
-              id="email"
-              onChange={this.handleChange} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="pwd">Password:</label>
-            <input className="form-control"
-              placeholder="Password goes here..."
-              name="password"
-              type="password"
-              id="pwd"
-              onChange={this.handleChange} />
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-        <p><Link to="/login">Go to Login</Link></p>
+      <div>
+      <div className="row">
+        <div className="container col-md-6 signup-container">
+          <h1>JOIN THE MOVEMENT </h1>
+          <h2>SIGN UP NOW FOR FREE</h2>
+          <form onSubmit={this.handleFormSubmit}>
+            <div className="form-group">
+              <label htmlFor="username">Username:</label>
+              <input className="form-control"
+                placeholder="Username goes here..."
+                name="username"
+                type="text"
+                id="username"
+                onChange={this.handleChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email address:</label>
+              <input className="form-control"
+                placeholder="Email goes here..."
+                name="email"
+                type="email"
+                id="email"
+                onChange={this.handleChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="pwd">Password:</label>
+              <input className="form-control"
+                placeholder="Password goes here..."
+                name="password"
+                type="password"
+                id="pwd"
+                onChange={this.handleChange} />
+            </div>
+            <button type="submit" className="btn btn-danger">Signup</button>
+          </form>
+          
+        </div>
+      </div>
+      <div className="row">
+      <div className="container col-md-6">
+      </div>
+      </div>
       </div>
     );
   }
