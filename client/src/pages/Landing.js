cf5cfb0e692fd7a76b44e4fcee81aee7ec5d38a2
Clark Tyra <clark.tyra@gmail.com>
2019-08-23 17:52:32 -0400

    made a styled component for image

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import apartment from '../images/apartment.jpg'
+import styled from 'styled-components';
+import './Landing.css';
+
+const Picture = styled.img`
+    width: 100%;
+    border-radius: 0;
+    background-color: blue;
+
+
+`;
+
 
 class Landing extends Component {
     render() {
         return (
             <div>
-            Landing
+            <Picture src={apartment}/>
             </div>
         )
     }
