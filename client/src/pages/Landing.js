ac99386374bd35dd3b8bb50a2e6a104d7c800eb5
Clark Tyra <clark.tyra@gmail.com>
2019-09-04 11:24:54 -0700

    added columns-rows with text, put the picture in the container

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -16,7 +16,24 @@ class Landing extends Component {
     render() {
         return (
             <div>
-            <Picture src={apartment}/>
+                
+                <div class="container">
+                <Picture src={apartment} />
+                    <div class="row">
+                        <div class="col-sm">
+                            What to expect:
+                            Burst Workouts gives you access to a unique short workout to complete every day
+                        </div>
+                        <div class="col-sm">
+                            The Burst Streak method: 
+                             Each consecutive workout adds to your Burst Streak, which improves your workout consistency!
+                        </div>
+                        <div class="col-sm">
+                            Online comunity:
+                            Challenge your friends and our online comunity for the longest streaks 
+                         </div>
+                    </div>
+                </div>
             </div>
         )
     }
