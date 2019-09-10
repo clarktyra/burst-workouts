94512a502ba3fa97e9783bc4b6e458c907bd0226
Clark Tyra <clark.tyra@gmail.com>
2019-09-10 14:48:49 -0700

    connected totalWorkouts to the backend

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -21,98 +21,84 @@ class Home extends Component {
       email: "",
       currentDaysInRow: null,
       highestDaysInRow: null,
-      totalDaysInRow: 3,
+      totalDaysInRow: null,
       collapse: false
     };
     this.toggle = this.toggle.bind(this);
   }
-  
+
 
   componentDidMount() {
     API.getUser(this.props.user.id).then(res => {
       this.setState({
         username: res.data.username,
-        email: res.data.email, 
-        currentDaysInRow: res.data.currentStreak, 
-        highestDaysInRow: res.data.longestStreak
+        email: res.data.email,
+        currentDaysInRow: res.data.currentStreak,
+        highestDaysInRow: res.data.longestStreak,
+        totalDaysInRow: res.data.totalWorkouts
       })
     });
   }
-  
+
   toggle() {
-    this.setState( {collapse: !this.state.collapse });
+    this.setState({ collapse: !this.state.collapse });
   }
 
-  setHover = () => {this.setState({hovered: true})}
-  cancelHover = () => {this.setState({hovered: false})}
+  setHover = () => { this.setState({ hovered: true }) }
+  cancelHover = () => { this.setState({ hovered: false }) }
 
-  
+
 
 
   render() {
     return (
       <div>
-        
         <div className="container">
-        <h1 id="welcomeTitle">Welcome {this.state.username},</h1>
+          
+          <h1 id="welcomeTitle">Welcome {this.state.username},</h1>
+          
           <div className="row">
+            
             <div className="col-sm">
               <Card id="first-card" className="card">
                 <CardBody>
                   <CardTitle style={titleStyle}>{this.state.currentDaysInRow}</CardTitle>
-                  <CardText> You have done a burst workout {this.state.currentDaysInRow} 
-                  {(this.state.currentDaysInRow) > 1 ? " days " : " day "}
-                  in a row
-                  </CardText>
+                  <CardText> You have done a burst workout {this.state.currentDaysInRow} {(this.state.currentDaysInRow) > 1 ? " days " : " day "} in a row</CardText>
                 </CardBody>
               </Card>
             </div>
-
+            
             <div className="col-sm">
               <Card id="middle-card" className="card">
                 <CardBody>
                   <CardTitle style={titleStyle}>{this.state.highestDaysInRow}</CardTitle>
-                  <CardText> Your longest workout streak is {this.state.highestDaysInRow} 
-                  {(this.state.highestDaysInRow) > 1 ? " days " : " day "}
-                  </CardText>
+                  <CardText> Your longest workout streak is {this.state.highestDaysInRow} {(this.state.highestDaysInRow) > 1 ? " days " : " day "}</CardText>
                 </CardBody>
               </Card>
             </div>
-
+            
             <div className="col-sm">
               <Card id="last-card" className="card">
                 <CardBody>
-                  <CardTitle style={titleStyle}>{this.state.totalDaysInRow}</CardTitle>
-                  <CardText> Your total number of burst workouts is {this.state.totalDaysInRow}
-                  </CardText>
+                 <CardTitle style={titleStyle}>{this.state.totalDaysInRow}</CardTitle>
+                 <CardText> Your total number of burst workouts is {this.state.totalDaysInRow}</CardText>
                 </CardBody>
               </Card>
             </div>
-          </div>
-          <br/>
-        <br/>
-          
-          <Link to="/workout"><Button className="homeButtons" color="warning">Click here for today's workout </Button></Link>
-
             
+          </div>       
+          <br/> 
+          <Link to="/workout"><Button className="homeButtons" color="warning">Click here for today's workout </Button></Link>
+          <br/>
+          <br/>
+          <Button className="homeButtons" color="danger" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Exercise tidbit of the day!</Button>
+          <Collapse isOpen={this.state.collapse}>
+            <Card>
+              <CardBody>It is not about how hard you workout today. Its how consistent you have been in a lifetime.</CardBody>
+            </Card>
+          </Collapse>
           
-          <br/>
-          <br/>
-
-        <Button className="homeButtons" color="danger" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Exercise tidbit of the day!</Button>
-        <Collapse isOpen={this.state.collapse}>
-          <Card>
-            <CardBody>
-            It is not about how hard you workout today. Its how consistent you have been in a lifetime.
-            </CardBody>
-          </Card>
-        </Collapse>
         </div>
-        <br />
-
-        
-        
-
       </div>
     )
   }
